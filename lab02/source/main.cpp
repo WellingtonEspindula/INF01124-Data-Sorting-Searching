c035b517e372fb4fe09004be04a279c5e014a663
Wellington Espindula <wellington.m.espindula@gmail.com>
2019-10-10 01:26:04 -0300

    Lab02 is done

--- lab02/source/main.cpp
+++ lab02/source/main.cpp
@@ -15,6 +15,7 @@
 #include <cstring>
 #include <ctime>
 #include <iostream>
+#include <fstream>
 #include <stack>
 #include <vector>
 
@@ -25,123 +26,117 @@
 
 using namespace std;
 
-int main(){
+// prototypes
+vector<vector<int>> readFileToVectors(string fileName);
+void writeList(string fileName, vector<vector<int>> list);
+void writeStats(string fileName, vector<QuickSortStats> stats);
+
+int main(int argc, char ** argv){
     srand(time(0));
-
-    // vector<int> v1 = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
-    // vector<int> v1 = {1, 3, 2, 4};
-    vector<int> v1 = {5, 4, 3, 2, 1};
-    QuickSortStats v1_stats;
-    quick_sort(&v1, RANDOM, &v1_stats);
-
-    for (int i = 0; i < v1.size(); i++){
-        printf("%d ", v1.at(i));
+    
+    vector<vector<int>> lists;
+    
+    if (argc >= 1){
+        lists = readFileToVectors(argv[1]);
+    } else {
+        lists = readFileToVectors("entrada.txt");
     }
-    printf("\n");
 
-/*
-    // First list to test
-    int test1_size = 9;
+    vector<vector<int>> ordered_med_lists = vector<vector<int>>();
+    vector<vector<int>> ordered_rand_lists = vector<vector<int>>();
 
-    // Using shell sort
-    printf("Shell Sort: \n");
-    int test1_shellsort[test1_size] = {54, 26, 93, 17, 77, 31, 44, 55, 20};
-    shell_insertion_sort(test1_shellsort, test1_size, true, true);
-    printf("\n");
+    vector<QuickSortStats> med_stats_list;
+    vector<QuickSortStats> rand_stats_list;
 
-    // Same test using ciura sequence shell sort
-    printf("Ciura Sequence Shell Sort: \n");
-    int test1_ciura[test1_size] = {54, 26, 93, 17, 77, 31, 44, 55, 20};
-    ciura_sequence_shell_sort(test1_ciura, test1_size, true, true);
-    printf("\n");
+    for (int i = 0; i < lists.size(); i++){
+        vector<int> v_med = lists.at(i);
+        QuickSortStats v_med_stats;
+        
+        vector<int> v_rand = v_med;
+        QuickSortStats v_rand_stats;
+        
+        quick_sort(&v_med, MEDIAN, &v_med_stats);
+        quick_sort(&v_rand, MEDIAN, &v_rand_stats);
 
+        ordered_med_lists.push_back(v_med);
+        med_stats_list.push_back(v_med_stats);
 
+        ordered_rand_lists.push_back(v_rand);
+        rand_stats_list.push_back(v_rand_stats);
 
-    // Second list to test
-    int test2_size = 15;
+    }
 
-    // Using Shell sort
-    printf("Shell Sort: \n");
-    int test2_shellsort[test2_size] = {733, 77, 611, 51, 496, 990, 61, 175, 127, 794, 794, 76, 197, 141, 863};
-    shell_insertion_sort(test2_shellsort, test2_size, true, true);
-    printf("\n");
-
-    // Using Ciura shell sort
-    printf("Ciura Sequence Shell Sort: \n");
-    int test2_ciura[test2_size] = {733, 77, 611, 51, 496, 990, 61, 175, 127, 794, 794, 76, 197, 141, 863};
-    ciura_sequence_shell_sort(test2_ciura, test2_size, true, true);
-    printf("\n");
-
-
-
-    // Third list to test
-    int test3_size = 20;
-
-    // Using Shell sort
-    printf("Shell Sort: \n");
-    int test3_shellsort[test3_size] = {872, 424, 729, 573, 877, 696, 321, 758, 949, 217, 767, 238, 562, 175, 999, 12, 380, 406, 225, 995};
-    shell_insertion_sort(test3_shellsort, test3_size, true, true);
-    printf("\n");
-
-    // Using Ciura shell sort
-    printf("Ciura Sequence Shell Sort: \n");
-    int test3_ciura[test3_size] = {872, 424, 729, 573, 877, 696, 321, 758, 949, 217, 767, 238, 562, 175, 999, 12, 380, 406, 225, 995};
-    ciura_sequence_shell_sort(test3_ciura, test3_size, true, true);
-    printf("\n");
-
-
-    // Random number tests
-    int size1 = 100;
-    int random_list1[size1];
-    int cp_random_list1[size1];
-    random_list(random_list1, size1);
-    memcpy(cp_random_list1, random_list1, (size1*sizeof(int)));
-
-    printf("Size = 100: \n");
-    printf("Shell: ");
-    shell_insertion_sort(random_list1, size1, false, true);
-    printf("Ciura: ");
-    ciura_sequence_shell_sort(random_list1, size1, false, true);
-
-
-    int size2 = 1000;
-    int random_list2[size2];
-    int cp_random_list2[size2];
-    random_list(random_list2, size2);
-    memcpy(cp_random_list2, random_list2, (size2*sizeof(int)));
-
-    printf("\nSize = 1000: \n");
-    printf("Shell: ");
-    shell_insertion_sort(random_list2, size2, false, true);
-    printf("Ciura: ");
-    ciura_sequence_shell_sort(random_list2, size2, false, true);
-
-
-    int size3 = 10000;
-    int random_list3[size3];
-    int cp_random_list3[size3];
-    random_list(random_list3, size3);
-    memcpy(cp_random_list3, random_list3, (size3*sizeof(int)));
-
-    printf("\nSize = 10000: \n");
-    printf("Shell: ");
-    shell_insertion_sort(random_list3, size3, false, true);
-    printf("Ciura: ");
-    ciura_sequence_shell_sort(random_list3, size3, false, true);
-
-
-    int size4 = 10000;
-    int random_list4[size4];
-    int cp_random_list4[size4];
-    random_list(random_list4, size4);
-    memcpy(cp_random_list4, random_list4, (size4*sizeof(int)));
-
-
-    printf("\nSize = 100000: \n");
-    printf("Shell: ");
-    shell_insertion_sort(random_list4, size4, false, true);
-    printf("Ciura: ");
-    ciura_sequence_shell_sort(random_list4, size4, false, true); */
+    writeList("saida-primeiro.txt", ordered_med_lists);
+    writeStats("stats-primeiro.txt", med_stats_list);
+    
+    writeList("saida-aleatorio.txt", ordered_rand_lists);
+    writeStats("stats-aleatorio.txt", rand_stats_list);
 
     return 0;
+}
+
+vector<vector<int>> readFileToVectors(string fileName){
+    vector<vector<int>> vectors = vector<vector<int>>();
+
+    ifstream file (fileName);
+    if (file.is_open()){
+        string nlists;
+        getline (file, nlists);
+
+        int numberOfLists = stoi(nlists);
+
+        for (int i = 0; i < numberOfLists; i++){
+            string nNum;
+            getline(file, nNum, ' ');
+            int numberOfNumers = stoi(nNum);
+
+            if (numberOfNumers !=  0){
+                vector<int> list = vector<int>();
+
+                string str_number;
+                for (int j = 0; j < numberOfNumers - 1; j++){
+                    getline(file, str_number, ' ');
+                    int number = stoi(str_number);
+                    list.push_back(number);
+                }
+                getline(file, str_number, '\n');
+                int number = stoi(str_number);
+                list.push_back(number);
+
+                vectors.push_back(list);
+            }
+        }
+        file.close();
+    }
+    return vectors;
+} 
+
+void writeList(string fileName, vector<vector<int>> list){
+    ofstream file (fileName);
+    if (file.is_open())
+    {
+        file << list.size() << "\n";
+        for (int i = 0; i < list.size(); i++){
+            file << list.at(i).size() << " ";
+            for (int j = 0; j < (list.at(i).size() - 1); j++){
+                file << list.at(i).at(j) << " ";
+            }
+            file << list.at(i).back() << "\n";
+        }      
+        file.close();
+    }
+ 
+}
+
+
+void writeStats(string fileName, vector<QuickSortStats> stats){
+    ofstream file(fileName);
+    if (file.is_open())
+    {
+        for (int i = 0; i < stats.size(); i++){
+            file << "[swaps: " << stats.at(i).swaps << "; recursive calls: " << stats.at(i).recursiveCalls 
+                    << "; execution time: " << stats.at(i).executionTime << " s]"<< "\n";
+        }
+        file.close();
+    }
 }
